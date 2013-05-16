c5be32b14671e506d84d4ce9765d62dcb6840bd1
Sumita Sami <sumitasami@Sumitas-MacBook-Air.local>
2013-05-16 07:41:54 -0700

    HTML crawler and scraper

--- pyMisc/scrape_html.py
+++ pyMisc/scrape_html.py
@@ -1,13 +1,19 @@
 import requests, re
 
-#### Learning how to do regexes in Python 
-
-def findAllLinks() : 
-	"""Finding all the links in a document"""
-	r = requests.get("http://www.google.com");
-	p = re.compile('<a.*?>.*?</a>') 
-	match = p.findall(r.content)
-	for i in match: 
-		print "here's this", i
+#### Learning how to do regexes in Python
 
 
+def findAllLinks( url ) :
+  """Finding all the links in a document"""
+  r = requests.get(url);
+  p = re.compile('<a.*?>.*?</a>')
+  match = p.findall(r.content)
+  links = []
+  for i in match:
+    links.append(i)
+
+  return links
+
+#links = findAllLinks("http://www.chris-granger.com/resume/")
+#for i in links:
+#    print "This url is: ", i
