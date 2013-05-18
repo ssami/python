db4d394f8a6361bafe59faabd470986919f16213
ssami <ssami@ebay.com>
2013-05-17 17:33:27 -0700

    Html scraper

--- pyMisc/scrape_html.py
+++ pyMisc/scrape_html.py
@@ -14,6 +14,30 @@ def findAllLinks( url ) :
 
   return links
 
+
+def cleanHtml( text ):
+    pattBreaks = re.compile('<\/?br>')
+    pattBreaks.sub('\n', text)
+    pattBreaks = re.compile('<\/?p>')
+    pattBreaks.sub('\n', text)
+    f1 = open('debug1.txt', 'w')
+    f1.write(text)
+    pattBreaks = re.compile('\.|\?|\!\"')
+    pattBreaks.sub('\n', text)
+    f2 = open('debug2.txt', 'w')
+    f2.write(text)
+    pattTags = re.compile('<.*?>')
+    pattTags.sub('', text)
+    f3 = open('debug3.txt', 'w')
+    f3.write(text)
+    
+    return text.split('\n')
+
+
 #links = findAllLinks("http://www.chris-granger.com/resume/")
 #for i in links:
 #    print "This url is: ", i
+
+
+origText = requests.get('http://docs.python.org/2/library/re.html').content
+cleanText = cleanHtml(origText)
